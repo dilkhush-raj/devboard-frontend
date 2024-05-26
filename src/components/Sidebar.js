7b027413fbd87ac9ac7c51acaaf0a55302f9cc52
Dilkhush Raj <dilkhush_raj@outlook.com>
2024-05-26 22:51:51 +0530

    Add prettier and prettier confit, update navbar button to resolve build error

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,9 @@
 "use client";
-import { navLinks } from "@/constants";
-import { usePathname } from "next/navigation";
+import {navLinks} from "@/constants";
+import {usePathname} from "next/navigation";
 import Link from "next/link";
-import { useState } from "react";
-import { IoMenu, IoClose } from "react-icons/io5";
+import {useState} from "react";
+import {IoMenu, IoClose} from "react-icons/io5";
 
 export default function Sidebar() {
   const [open, setOpen] = useState(false);
@@ -15,38 +15,26 @@ export default function Sidebar() {
           open ? "" : " translate-x-[-100%] "
         } z-30 md:translate-x-0 fixed transition-transform duration-300 flex flex-col gap-4 justify-between top-[60px] bottom-0 p-4 left-0 h-[calc(100vh-60px)] border-r dark:border-dark-400 border-border-100 dark:border-darkColor-400 w-[220px] overflow-y-auto bg-lightColor-900 dark:bg-darkColor-200 `}
       >
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-3">
           {navLinks.map((item) => {
             const isActive =
               (pathname.includes(item.slug) && item.slug.length > 1) ||
               pathname === item.slug;
             return (
-              <Link
-                key={item?.slug}
-                href={item?.slug}
-                onClick={() => setOpen(false)}
-                className={`${
-                  isActive ? " bg-gradient-primary text-lightColor-900 " : ""
-                } flex items-center hover:bg-lightColor-700 dark:hover:bg-darkColor-400 justify-start gap-4 bg-transparent py-3 px-4 rounded-md `}
-              >
-                {item?.icon}
-                {item?.label}
-              </Link>
+              <div key={item?.slug}>
+                <Link
+                  href={item?.slug}
+                  onClick={() => setOpen(false)}
+                  className={`${
+                    isActive ? " bg-gradient-primary text-lightColor-900 " : ""
+                  } flex items-center hover:bg-lightColor-700 dark:hover:bg-darkColor-400 justify-start gap-4 bg-transparent py-3 px-4 rounded-md `}
+                >
+                  {item?.icon}
+                  {item?.label}
+                </Link>
+              </div>
             );
           })}
-        </div>
-        {/* Update below buttons */}
-        <div className="flex flex-col gap-2">
-          <Link href={"/login"} className="flex items-center justify-center gap-4 px-4 py-3 border rounded-md bg-lightColor-800 dark:bg-darkColor-300 border-border-100 ">
-            <button >
-              Log In
-            </button>
-          </Link>
-          <Link href={"/signup"} className="flex items-center justify-center gap-4 px-4 py-3 rounded-md dark:bg-darkColor-400 bg-lightColor-600 ">
-            <button>
-              Sign Up
-            </button>
-          </Link>
         </div>
       </div>
       <div
