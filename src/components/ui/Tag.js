00dee6406cfadf3148eb1ea99c9cdde37f999744
Dilkhush Raj <dilkhush_raj@outlook.com>
2024-06-29 12:09:55 +0530

    Update Tag component and add tracing-beam for blog

--- src/components/ui/Tag.js
+++ src/components/ui/Tag.js
@@ -1,7 +1,7 @@
 const Tag = ({children}) => {
   return (
     <div
-      className={`w-max cursor-pointer rounded-md bg-lightColor-750 px-4 py-2 text-xs text-darkColor-400 shadow-sm dark:bg-shadow-200 dark:text-lite-700`}
+      className={`w-max cursor-pointer rounded-md border border-border-100 bg-lightColor-750 px-4 py-2 text-xs text-darkColor-400 shadow-sm dark:border-darkColor-400 dark:bg-shadow-200 dark:text-lite-700`}
     >
       {children}
     </div>
