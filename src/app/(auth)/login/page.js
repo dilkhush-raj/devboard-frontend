52848dcec9845fdfd3ea7b43077457b5e6ef8dbe
Dilkhush Raj <dilkhush_raj@outlook.com>
2024-06-29 12:05:52 +0530

    Update login page

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -21,7 +21,7 @@ export default function LoginForm() {
   const onSubmit = async (data) => {
     try {
       const response = await axios.post(
-        "http://localhost:8000/api/v1/users/login",
+        `${process.env.NEXT_PUBLIC_BACKEND_SERVER_BASE_URL}/api/v1/users/login`,
         {
           username: data.username,
           password: data.password,
@@ -43,8 +43,8 @@ export default function LoginForm() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-[url('/loginbg.jpg')] bg-cover bg-center px-4">
-      <div className="dark:bg-darkColor-300 w-4xl border-border-100 dark:border-darkColor-400 rounded-lg border bg-white p-4 sm:w-[500px]">
-        <h2 className="border-border-100 dark:border-darkColor-400 mb-4 flex items-center justify-center gap-2 border-b pb-4 text-center text-xl font-medium">
+      <div className="w-4xl rounded-lg border border-border-100 bg-white p-4 dark:border-darkColor-400 dark:bg-darkColor-300 sm:w-[500px]">
+        <h2 className="mb-4 flex items-center justify-center gap-2 border-b border-border-100 pb-4 text-center text-xl font-medium dark:border-darkColor-400">
           Log In to <Logo />
         </h2>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
@@ -56,7 +56,7 @@ export default function LoginForm() {
                 pattern: /^[a-zA-Z0-9-_]+$/,
                 minLength: 3,
               })}
-              className="border-border-100 dark:border-darkColor-400 w-full rounded-md border px-4 py-2"
+              className="w-full rounded-md border border-border-100 px-4 py-2 dark:border-darkColor-400"
             />
             {errors.username && (
               <span className="text-xs text-red-500">
@@ -72,11 +72,11 @@ export default function LoginForm() {
               <input
                 {...register("password", {required: true, minLength: 6})}
                 type={showPassword ? "text" : "password"}
-                className="border-border-100 dark:border-darkColor-400 w-full rounded-md border px-4 py-2"
+                className="w-full rounded-md border border-border-100 px-4 py-2 dark:border-darkColor-400"
               />
               <button
                 type="button"
-                className="dark:text-darkColor-400 absolute inset-y-0 right-0 px-4 py-2 text-[#ddd]"
+                className="absolute inset-y-0 right-0 px-4 py-2 text-[#ddd] dark:text-darkColor-400"
                 onClick={() => setShowPassword(!showPassword)}
               >
                 {showPassword ? <IoEye /> : <IoEyeOff />}
@@ -91,7 +91,7 @@ export default function LoginForm() {
 
           <button
             type="submit"
-            className="border-border-100 dark:border-darkColor-400 bg-gradient-primary w-full rounded-md py-2 text-white hover:bg-blue-600"
+            className="hover:bg-blue-600 w-full rounded-md border-border-100 bg-gradient-primary py-2 text-white dark:border-darkColor-400"
           >
             Log In
           </button>
