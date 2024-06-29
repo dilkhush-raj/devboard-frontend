7e6d0b5b72820156f5b924eab05a1ce376d96885
Dilkhush Raj <dilkhush_raj@outlook.com>
2024-06-29 12:08:21 +0530

    Update BlogCard

--- src/components/shared/BlogCard.js
+++ src/components/shared/BlogCard.js
@@ -6,6 +6,8 @@ import {FaCommentAlt} from "react-icons/fa";
 import MDEditor from "@uiw/react-md-editor";
 import ShareButtons from "../ui/ShareButton";
 import CardMenu from "../ui/CardMenu";
+import {FaCalendarAlt} from "react-icons/fa";
+import {Button} from "@nextui-org/button";
 
 /**
  * BlogCard component to display a blog card with author details, like/dislike functionality, comments, and sharing options.
@@ -26,6 +28,8 @@ import CardMenu from "../ui/CardMenu";
  * @param {number} props.comment[].id - Unique identifier for the comment.
  * @param {string} props.comment[].user - User who made the comment.
  * @param {string} props.comment[].data - Content of the comment.
+ * @param {string[]} props.tags - Array of tags associated with the blog.
+ * @param {string} props.banner - URL of the banner image.
  * @returns {JSX.Element} Rendered BlogCard component.
  */
 
@@ -37,59 +41,94 @@ export default function BlogCard({
   author_username,
   author_profile_img,
   content,
+  banner,
   like = 0,
   dislike = 0,
   comment = [],
+  tags = [],
 }) {
+  // content to show 100 characters
+  const contentToShow = content.slice(0, 200) + "...";
   return (
-    <div className="relative rounded-lg bg-white p-4 shadow-sm dark:bg-darkColor-300">
-      <div className="flex flex-col gap-4">
-        <div>
-          <div className="flex items-center justify-between">
-            <Link
-              href={`/${author_username}`}
-              className="flex items-center gap-2"
-            >
-              <Image
-                src={author_profile_img || "/avatar.jpg"}
-                width={40}
-                height={40}
-                className="rounded-full"
-                alt={author}
-              />
-              <div className="flex flex-col justify-center">
-                <div className="text-sm">{author}</div>
-                <div className="text-xs">{author_username}</div>
-              </div>
-              <li className="pl-4 text-xs">{published_at}</li>
-            </Link>
-            <CardMenu />
-          </div>
-        </div>
+    // <div className="relative rounded-lg border border-border-100 bg-white p-4 shadow-sm dark:border-darkColor-400 dark:bg-darkColor-300">
+    //   <div className="flex flex-col gap-4">
+    //     <div className="flex items-center justify-between border-b border-border-100 pb-4 dark:border-darkColor-400">
+    //       <div className="flex items-center gap-2">
+    //         <Link
+    //           href={`/@${author_username}`}
+    //           className="flex items-center gap-2"
+    //         >
+    //           <img
+    //             src={author_profile_img || "/avatar.jpg"}
+    //             width={40}
+    //             height={40}
+    //             className="h-[40px] w-[40px] rounded-full object-cover"
+    //             alt={author}
+    //           />
+    //           <div className="flex flex-col justify-center">
+    //             <div className="text-sm">{author}</div>
+    //             <div className="text-xs text-darkColor-150 dark:text-zinc-400">
+    //               @{author_username}
+    //             </div>
+    //           </div>
+    //         </Link>
+    //         <div className="flex items-center gap-2 pl-4 text-sm">
+    //           <FaCalendarAlt /> {published_at}
+    //         </div>
+    //       </div>
+    //       <CardMenu />
+    //     </div>
+    //     <img src={banner} alt="banner" className="h-auto w-full" />
 
-        <h2 className="text-xl font-bold dark:text-white">{title}</h2>
+    //     <h2 className="text-xl font-bold dark:text-white">{title}</h2>
 
-        <MDEditor.Markdown className="select-text" source={content} />
-        <div className="flex items-center gap-4 text-sm">
-          <div className="flex w-max cursor-pointer items-center gap-2 rounded-full bg-lightColor-700 px-3 py-1 dark:bg-darkColor-400">
-            <div className="flex items-center gap-2 hover:text-primary-500">
-              <PiArrowFatUpFill />
-              {like}
-            </div>
-            <div className="flex items-center gap-2 hover:text-red-600">
-              <PiArrowFatDownFill />
-              {dislike}
-            </div>
-          </div>
-          <div className="flex w-max cursor-pointer items-center gap-2 rounded-full bg-lightColor-700 px-3 py-1 hover:text-cyan-600 dark:bg-darkColor-400">
-            <div className="flex items-center gap-2">
-              <FaCommentAlt />
-              <div>{comment?.length}</div>
-            </div>
-          </div>
-          <div className="relative flex w-max cursor-pointer items-center gap-2 rounded-full bg-lightColor-700 px-3 py-1 dark:bg-darkColor-400">
-            <ShareButtons url={`/blogs/${slug}`} title={title} />
-          </div>
+    //     <MDEditor.Markdown className="select-text" source={content} />
+
+    //     <div className="flex flex-wrap gap-2">
+    //       {tags.map((tag) => (
+    //         <div key={tag._id} className="text-sm">
+    //           #{tag?.name}
+    //         </div>
+    //       ))}
+    //     </div>
+
+    //     <div className="flex items-center gap-4 text-sm">
+    //       <div className="flex w-max cursor-pointer items-center gap-2 rounded-full bg-lightColor-700 px-3 py-1 dark:bg-darkColor-400">
+    //         <div className="flex items-center gap-2 hover:text-primary-500">
+    //           <PiArrowFatUpFill />
+    //           {like}
+    //         </div>
+    //         <div className="flex items-center gap-2 hover:text-red-600">
+    //           <PiArrowFatDownFill />
+    //           {dislike}
+    //         </div>
+    //       </div>
+    //       <div className="flex w-max cursor-pointer items-center gap-2 rounded-full bg-lightColor-700 px-3 py-1 hover:text-cyan-600 dark:bg-darkColor-400">
+    //         <div className="flex items-center gap-2">
+    //           <FaCommentAlt />
+    //           <div>{comment?.length}</div>
+    //         </div>
+    //       </div>
+    //       <div className="relative flex w-max cursor-pointer items-center gap-2 rounded-full bg-lightColor-700 px-3 py-1 dark:bg-darkColor-400">
+    //         <ShareButtons url={`/blogs/${slug}`} title={title} />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="relative grid min-h-[240px] rounded-lg border border-border-100 bg-white p-4 shadow-sm dark:border-darkColor-400 dark:bg-darkColor-300">
+      <div className="grid gap-4 sm:grid-cols-[2fr_3fr]">
+        <img src={banner} alt="image" className="h-full w-full object-cover" />
+        <div className="flex flex-col gap-2">
+          <h2 className="border-b border-border-100 pb-2 text-xl font-bold dark:border-darkColor-400">
+            {title}
+          </h2>
+          <div className="text-sm">{contentToShow}</div>
+          <div>Author: {author}</div>
+          <Link href={`/blogs/${slug}`}>
+            <Button className="rounded-lg bg-gradient-primary text-white">
+              Read
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
